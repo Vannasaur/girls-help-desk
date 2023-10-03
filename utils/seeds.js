f9f73fab82d9ef17f152bf6ac6d8c4c69768cd74
maliacho <maliacho21@gmail.com>
2023-10-03 14:36:22 -0700

    added layout for optional todos in task

--- utils/seeds.js
+++ utils/seeds.js
@@ -4,6 +4,10 @@ const { User } = require('../models');
 // @@TODO userData.json will be in this same folder
 const userData = require('./userData.json')
 
+// OPTIONAL @@ TODO create sample ticketData.json in this same folder
+const ticketData = require('./ticketData.json');
+
+// seed sample User data
 const seedDatabase = async () => {
 
     await sequelize.sync({ force: true });
@@ -17,4 +21,12 @@ const seedDatabase = async () => {
 };
 
 seedDatabase();
+
+// OPTIONAL seed sample Ticket data
+
+
+
+
+
+// seed sample Log data
 
