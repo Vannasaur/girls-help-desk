f652f649326d53c102a3d2ef136b6ab30e54e981
GioLucci <141793843+Vannasaur@users.noreply.github.com>
2023-10-05 11:54:16 -0700

    updated format date and format timestamp, safe revert point

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,12 @@
 module.exports = {
     formatTimestamp: (date) => {
-        return date.toLocaleTimeString();
+        let timeStamp = new Date(date);
+        let hour = timeStamp.getHours();
+        let minutes = timeStamp.getMinutes();
+        let meridiem = hours >= 12 ? 'PM' : 'AM';
+        hour = hour % 12;
+        hour = hour ? hour : 12
+        return `${hour}:${minutes} ${meridiem}`
     },
 
     withAuth: (req, res, next) => {
@@ -13,7 +19,16 @@ module.exports = {
 
         format_date: (date) => {
             // Format date as  Mo DD, YYYY hh:mm A format.
-            return date.toLocaleDateString();
+            let timeStamp = new Date(date);
+            let monthData = timeStamp.getMonth();
+            const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+            let month = months[monthData];
+            let day = timeStamp.getDate();
+            let year = timeStamp.getFullYear();
+
+            const time = formatTimestamp(timeStamp)
+
+            return `${month} ${day} ${year} ${time}`
         },
 };
 
