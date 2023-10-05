f7e8558fd7cdd148b92d0aa9b5a7c42541460ba0
Drinkmilk <141216114+lndvng@users.noreply.github.com>
2023-10-05 11:19:55 -0700

    added comments to logroutes

--- routes/apiRoutes/logRoutes.js
+++ routes/apiRoutes/logRoutes.js
@@ -7,9 +7,12 @@ const {
 } = require('../../controllers/logControllers')
 
 router.route('/api/log/:ticketId?drawer=BOOLEAN')
+    // POST will call the createLog controller.
     .post(createLog);
 
 router.route('/api/log/:ticketId/:logId?drawer=BOOLEAN')
+    // PUT will call the editLog controller.
+    // DELETE will call the deleteLog controller.
     .put(editLog)
     .delete(deleteLog);
 
