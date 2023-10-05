6d6e707d864f95bbec3b5c60528b98c402c5eb8e
maliacho <maliacho21@gmail.com>
2023-10-05 11:55:18 -0700

    beginning work on findDiff function

--- utils/helpers.js
+++ utils/helpers.js
@@ -11,10 +11,18 @@ module.exports = {
         }
     },
 
-        format_date: (date) => {
-            // Format date as  Mo DD, YYYY hh:mm A format.
-            return date.toLocaleDateString();
-        },
+    format_date: (date) => {
+        // Format date as  Mo DD, YYYY hh:mm A format.
+        return date.toLocaleDateString();
+    },
+    
+    findDiff: (newValue, oldValue) => {
+        let activeUser;
+        if (oldValue === newValue) {
+            activeUser =
+            console.log(`${keyName} was changed from ${oldValue} to ${newValue} by ${activeUser}.`)
+        }
+    }
 };
 
 
